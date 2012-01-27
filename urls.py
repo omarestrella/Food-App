c9450c4b4a5e9d132d24fdb0bd3f58a29773c986
Omar Estrella <gzusfr3ak@gmail.com>
2012-01-27 13:30:56 -0500

    Commiting fix to a dumb mistake using style instead of link tag

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 from views import HomepageView
 
 urlpatterns = (
-    url(r'^', HomepageView.as_view(), name='url_homepage'),
+    url(r'^$', HomepageView.as_view(), name='url_homepage'),
 )
