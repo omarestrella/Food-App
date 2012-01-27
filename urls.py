c4aea30979b2ecf8a5cd64374ca5baeea61884b7
Omar Estrella <gzusfr3ak@gmail.com>
2012-01-27 13:21:25 -0500

    Added templates, urls, and initial migration

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from views import HomepageView
+
+urlpatterns = (
+    url(r'^', HomepageView.as_view(), name='url_homepage'),
+)
--- views.py
+++ views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+class HomepageView(TemplateView):
+    template_name = 'foodapp/homepage.html'
