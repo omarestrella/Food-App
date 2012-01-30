f5d21da6788d13efe574a469ed5171e6a1c36856
Tyler Cross <tyler@crosscollab.com>
2012-01-30 11:12:32 -0500

    migrations

--- forms.py
+++ forms.py
@@ -2,6 +2,6 @@ from django.forms import ModelForm
 from models import Order
 
 class OrderForm(ModelForm):
-	class Meta:
-		model = Order
-		exclude = ["user", "date"]
+    class Meta:
+        model = Order
+        exclude = ["user", "date"]
