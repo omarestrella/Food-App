aaa06f78fc64570d704b3d8164b20b19a113bc75
Tyler Cross <tyler@crosscollab.com>
2012-01-30 11:04:11 -0500

    Simplified the views.

--- views.py
+++ views.py
@@ -1,24 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.views.generic import TemplateView, ListView
+from django.views.generic import CreateView, ListView
 
 from models import Item, Order
 from forms import OrderForm
 
-class HomepageView(TemplateView):
-    template_name = 'foodapp/homepage.html'
-
-class ItemListView(ListView):
-	template_name = 'foodapp/items.html'
-	model = Item
-	context_object_name = 'items'
+class HomepageView(CreateView):
+    template_name = 'foodapp/placeOrder.html'
+    form_class = forms.OrderForm
+    success_url = 'foodapp/order.html'
 
 class OrderListView(ListView):
-	template_name = 'foodapp/orders.html'
-	model = Order
-	context_object_name = 'orders'
-
-class OrderFormView(FormView):
-	template_name = 'foodapp/placeOrder.html'
-	form_class = forms.OrderForm
-	success_url = 'directory'
+    template_name = 'foodapp/orders.html'
+    model = Order
+    context_object_name = 'orders'
